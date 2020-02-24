2d97c29d653257e8ea1d06e6c46d5ba05dd8c0a3
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-02-24 17:01:04 -0600

    completed mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
+  const params = useParams();
+  const id = params.id
  
   useEffect(() => {
-    const id = 1;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
-
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
+          console.log(response.data)
         })
         .catch(error => {
           console.error(error);
